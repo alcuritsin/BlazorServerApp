d42b7bcbe551a9d45c98c01721cfb123414362c6
Kuricyn Aleksey <al.curitsin@gmail.com>
2022-06-14 20:18:05 +0500

    Program.cs -mod

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using BlazorServerApp.Data;
+using BlazorServerApp.Data.Shop;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<ProductCatalogService>();
 
 var app = builder.Build();
 
